26b55d6ed4a51fee8e24e2648e289392779f5b1e
Ajay o s <akhubsmaster@gmail.com>
2022-08-08 06:01:22 +0000

    👨‍💻CODE BY 🕊️★⃝AJAY O S©️🧚‍♂️

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 //import React, { useEffect, useState } from "react";
-//import Loader from "./components/Load";
-import Home from "./www/Home";
+import Home from "./home.js";
 
 
 function App() {
-	//const [appLoaded, setAppLoaded] = useState(false);
-	//const [startLoadProgress, setStartLoadProgress] = useState(false);
-	//useEffect(() => {stopLoad();}, []);
-	//const stopLoad = () => {
-	//	setStartLoadProgress(true);
-	//	setTimeout(() => setAppLoaded(true), 3000);
-	//};
-	//if (!appLoaded) return <Loader done={startLoadProgress} />;
-
   return (
     <div className="App">
     	<Router>
     		<div className="app-content">
     			<Switch>
-        			<Route component={Home} />
+        			<Route component={ Home } />
       			</Switch>
     		</div>
   		</Router>
---
+++ src/home.js
@@ -0,0 +1,12 @@
+import "./home.css";
+import React from "react";
+
+const Home = () => {
+	return (
+		<div className="home">
+			
+		</div>
+	);
+};
+
+export default Home;
